cb8c1734a3c05cce947af7f04ce9834b64ba5770
Kyle Lange <baronsintrees@gmail.com>
2017-01-21 21:06:39 -0800

    add stopwatch styling.

--- app.js
+++ app.js
@@ -26,6 +26,19 @@ var PLAYERS = [
 
 var nextId = 4;
 
+var Stopwatch = React.createClass({
+  render: function() {
+    return (
+      <div className="stopwatch">
+        <h2>Stopwatch</h2>
+        <div className="stopwatch-time">0</div>
+        <button>Start</button>
+        <button>Reset</button>
+      </div>
+    );
+  }
+});
+
 var AddPlayerForm = React.createClass({
   propTypes: {
     onAdd: React.PropTypes.func.isRequired,
@@ -92,6 +105,7 @@ function Header(props) {
     <div className="header">
       <Stats players={props.players}/>
       <h1>{props.title}</h1>
+      <Stopwatch />
     </div>
   );
 }
